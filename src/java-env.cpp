e15bd36072e956159d90c7a236586b9dbfc49472
dingjing <dingjing@live.cn>
2025-02-11 20:07:32 +0800

    [FIX] 解决class依赖问题, 把所有依赖打入一个jar包

--- src/java-env.cpp
+++ src/java-env.cpp
@@ -5,6 +5,7 @@
 #include "java-env.h"
 
 #include <jni.h>
+#include <QDebug>
 
 
 class JavaEnvPrivate
@@ -26,13 +27,20 @@ bool JavaEnvPrivate::initJvm()
     JavaVMInitArgs jvmArgs = {};
     JavaVMOption   jvmOptions = {};
 
-    jvmOptions.optionString = const_cast<char*>("-Djava.class.path=.");
+    jvmOptions.optionString = const_cast<char*>("-Djava.class.path=.:/data/code/tika-wrap/tmp-dir/tika-main-1.0.0.jar");
     jvmArgs.version = JNI_VERSION_1_8;
     jvmArgs.nOptions = 1;
     jvmArgs.options = &jvmOptions;
     jvmArgs.ignoreUnrecognized = false;
 
     if (JNI_OK != JNI_CreateJavaVM(&mJvm, reinterpret_cast<void**>(&mJvmEnv), &jvmArgs)) {
+        qWarning() << "Create Java VM failed!";
+        return false;
+    }
+
+    jclass fullParserClass = mJvmEnv->FindClass("com/github/dingjingmaster/tika/main/FileOperation/FullParser");
+    if (nullptr == fullParserClass) {
+        qWarning() << "Find class 'FullParser' failed!";
         return false;
     }
 
@@ -62,6 +70,11 @@ JavaEnv* JavaEnv::getInstance()
 JavaEnv::JavaEnv()
     : QObject(nullptr), d_ptr(new JavaEnvPrivate(this))
 {
+    Q_D(JavaEnv);
+
+    if (!d->initJvm()) {
+        qWarning() << "JavaEnv init failed!";
+    }
 }
 
 JavaEnv::JavaEnv(const JavaEnv &)
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 //
 // Created by dingjing on 2/11/25.
 //
+#include "java-env.h"
+#include <QCoreApplication>
+
 int main (int argc, char* argv[])
 {
+    QCoreApplication app(argc, argv);
+
+    JavaEnv::getInstance();
+
     return 0;
 }
